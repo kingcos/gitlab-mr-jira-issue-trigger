aa75c94bbc9589f2179f85694087a49521686114
kingcos <2821836721v@gmail.com>
2018-09-26 00:40:58 +0800

    Add config struct

--- gitlab-ci-mr-jira-issue-trigger.go
+++ gitlab-ci-mr-jira-issue-trigger.go
@@ -1,9 +1,43 @@
 package main
 
-import (
-	"fmt"
-)
+// Config struct for the YAML file.
+type Config struct {
+	GitLab struct {
+		Host  string `yaml:"host"`
+		Token string `yaml:"token"`
+	} `yaml:"GitLab"`
+
+	Jira struct {
+		Host     string `yaml:"host"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"Jira"`
+
+	Server struct {
+		Port string `yaml:"port"`
+		Path string `yaml:"path"`
+	} `yaml:"Server"`
+
+	Trigger struct {
+		Merged struct {
+			ID      string `yaml:"id"`
+			Message string `yaml:"message"`
+		} `yaml:"merged"`
+		Opened struct {
+			ID      string `yaml:"id"`
+			Message string `yaml:"message"`
+		} `yaml:"opened"`
+		Closed struct {
+			ID      string `yaml:"id"`
+			Message string `yaml:"message"`
+		} `yaml:"closed"`
+		Locked struct {
+			ID      string `yaml:"id"`
+			Message string `yaml:"message"`
+		} `yaml:"locked"`
+	} `yaml:"Trigger"`
+}
 
 func main() {
-	
+
 }
